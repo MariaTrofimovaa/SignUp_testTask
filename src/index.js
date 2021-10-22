74c21478371980957806b14996585d3926d840be
MariaTrofimovaa <73877805+MariaTrofimovaa@users.noreply.github.com>
2021-10-22 14:24:04 +0300

    deploy settings added

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-import "./stylessheet/main.scss";
+import "./stylesheet/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
